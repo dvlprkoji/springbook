81a8e13154c3392aa21a218af53be309f984c2e8
koji <koji4321@naver.com>
2023-01-31 21:35:47 +0900

    1.8
    [문제점]
    - ConnectionMaker 구현 클래스를 선정하는 방식이 중복
    
    [해결]
    - ConnectionMaker 구현 클래스를 선정하는 코드를 메소드로 추출

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -3,14 +3,18 @@ package springbook.user.dao;
 public class DaoFactory {
 
     public UserDao userDao() {
-        return new UserDao(new DConnectionMaker());
+        return new UserDao(connectionMaker());
     }
 
     public AccountDao accountDao() {
-        return new AccountDao(new DConnectionMaker());
+        return new AccountDao(connectionMaker());
     }
 
     public MessageDao messageDao() {
-        return new MessageDao(new DConnectionMaker());
+        return new MessageDao(connectionMaker());
+    }
+
+    private ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 }
