8f96ef45b0424bcd737b3824c0ee9343fd7b98e4
koji <koji4321@naver.com>
2023-02-05 15:10:17 +0900

    2.1
    [문제]
    - 테스트시 생성한 User의 프로퍼티와 등록 후 조회한 User의 프로퍼티를 직접 비교하여 성공 여부를 판단해야 한다
    
    [해결]
    - 프로퍼티를 비교하는 코드를 추가하여 눈으로 성공, 실패를 확인할 수 있다

--- src/main/java/springbook/UserDaoTest.java
+++ src/main/java/springbook/UserDaoTest.java
@@ -10,7 +10,7 @@ public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException {
 
-        GenericXmlApplicationContext context = new GenericXmlApplicationContext("springbook/user/dao/applicationContext.xml");
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext("/applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
@@ -23,9 +23,13 @@ public class UserDaoTest {
         System.out.println(user.getId() + " 등록 성공");
 
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + " 조회 성공");
+        if (!user.getName().equals(user2.getName())) {
+            System.out.println("테스트 실패 (name)");
+        } else if (!user.getPassword().equals(user2.getPassword())) {
+            System.out.println("테스트 실패 (password)");
+        } else {
+            System.out.println("조회 테스트 성공");
+        }
     }
 }
