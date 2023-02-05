8fb44b124797dffdf3ac21cbe30fbed927caed3d
koji <koji4321@naver.com>
2023-02-05 18:58:02 +0900

    2.9
    [기능 추가]
    - 스프링을 이용한 테스트 적용 @RunWith(SpringJUnit4ClassRunner.class)
    - Set을 이용해 테스트마다 새로운 JUnitTest 객체를 생성하는 것을 확인

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -1,28 +1,49 @@
+import javafx.application.Application;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 public class JUnitTest {
-    
-    static JUnitTest testObject;
+
+    @Autowired
+    ApplicationContext context;
+
+    static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+    static ApplicationContext contextObject = null;
 
     @Test
     public void test1() {
-        assertThat(this, is(not(testObject)));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test
     public void test2() {
-        assertThat(this, is(not(testObject)));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test
     public void test3() {
-        assertThat(this, is(not(testObject)));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
 }
