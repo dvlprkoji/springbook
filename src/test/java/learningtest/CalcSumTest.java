750ffce5bf2eea9264f0229cd10257ec03f84f38
koji <koji4321@naver.com>
2023-02-09 21:36:46 +0900

    3.11
    [기능 추가]
    - 템플릿/콜백 예제 추가

---
+++ src/test/java/learningtest/CalcSumTest.java
@@ -0,0 +1,34 @@
+package learningtest;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class CalcSumTest {
+
+    @Test
+    public void sumOfNumbers() throws IOException {
+        Calculator calculator = new Calculator();
+        int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath());
+        assertThat(sum,is(10));
+    }
+
+    public class Calculator {
+        int calcSum(String filepath) throws IOException {
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Integer sum = 0;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sum += Integer.valueOf(line);
+            }
+            br.close();
+            return sum;
+        }
+    }
+
+}
