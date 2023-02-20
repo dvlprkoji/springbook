a522442fc0f0d79b3c765c7ac64bd30eaaec6cc6
koji <koji4321@naver.com>
2023-02-20 21:37:50 +0900

    5.10
    [문제점]
    - 템플릿/콜백 패턴을 사용하는 JdbcTemplate를 사용하지 못하고 직접 JDBC API를 직접 사용하는 방식으로 돌아가야 한다
    - 비즈니스 로직을 담고 있는 UserService에서 데이터 액세스 로직을 직접 다룬다
    - Connection을 다루기 때문에 데이터 액세스 기술이 변경될 경우 사용하지 못하게 된다
    
    [해결]
    - Spring이 제공하는 TransactionSynchronizationManager를 사용하여 직접 파라미터를 통해 Connection을 전달할 필요가 없도록 한다

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -1,16 +1,20 @@
 package springbook.user.service;
 
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import springbook.user.dao.UserDao;
 import springbook.user.dao.UserLevelUpgradePolicy;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
 public class UserService {
     UserDao userDao;
+    DataSource dataSource;
     UserLevelUpgradePolicy userLevelUpgradePolicy;
 
     public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
@@ -18,6 +22,10 @@ public class UserService {
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public void setUserLevelUpgradePolicy(UserLevelUpgradePolicy userLevelUpgradePolicy) {
@@ -32,31 +40,33 @@ public class UserService {
 //            }
 //        }
 //    }
-//    protected void upgradeLevel(User user) {
-//        user.upgradeLevel();
-//        userDao.update(user);
-//    }
+    protected void upgradeLevel(User user) {
+        user.upgradeLevel();
+        userDao.update(user);
+    }
 
     public void upgradeLevels() throws SQLException {
-        Connection c = null;
-        User user = null;
-        // ...
 
-        c.setAutoCommit(false); // Auto-Commit 해제
+        TransactionSynchronizationManager.initSynchronization();
+        Connection c = DataSourceUtils.getConnection(dataSource);
+        c.setAutoCommit(false);
+
         try {
-            // ...
-            upgradeLevel(c, user);
-            // ...
+            List<User> users = userDao.getAll();
+            for (User user : users) {
+                if (userLevelUpgradePolicy.canUpgradeLevel(user)) {
+                    upgradeLevel(user);
+                }
+            }
+            c.commit();
         } catch (SQLException e) {
             c.rollback();
             throw e;
+        } finally {
+            DataSourceUtils.releaseConnection(c, dataSource);
+            TransactionSynchronizationManager.unbindResource(this.dataSource);
+            TransactionSynchronizationManager.clearSynchronization();
         }
-        // ...
-    }
-
-    protected void upgradeLevel(Connection c, User user) throws SQLException{
-        user.upgradeLevel();
-        userDao.update(user);
     }
 
     public void add(User user) {
