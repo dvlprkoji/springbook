36f9ab89f51c343cc4a1694643605b23465ea5a2
koji <koji4321@naver.com>
2023-02-20 20:49:02 +0900

    5.8
    [기능 추가]
    - 업그레이드 도중 실패할 경우 롤백시키는 트랜잭션 기능을 확인하는 테스트코드 추가

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -31,7 +31,7 @@ public class UserService {
         }
     }
 
-    private void upgradeLevel(User user) {
+    protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
     }
--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -6,6 +6,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.dao.UserDao;
+import springbook.user.dao.UserLevelUpgradeBasicPolicy;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 import springbook.user.service.UserService;
@@ -14,6 +15,7 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static springbook.user.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
 import static springbook.user.service.UserService.MIN_RECOMMEND_FOR_GOLD;
 
@@ -30,21 +32,60 @@ public class UserServiceTest {
 
     List<User> users;
 
-    @Test
-    public void bean() {
-        assertThat(this.userService, is(notNullValue()));
+    static class TestUserService extends UserService {
+
+        private String id;
+
+        private TestUserService(String id) {
+            this.id = id;
+        }
+
+        @Override
+        protected void upgradeLevel(User user){
+            if (user.getId().equals(this.id)) throw new TestUserServiceException();
+            super.upgradeLevel(user);
+        }
+    }
+
+    static class TestUserServiceException extends RuntimeException {
     }
 
     @Before
     public void setUp() {
         users = Arrays.asList(
-                new User("koji1", "코지1", "p1", null, MIN_LOGCOUNT_FOR_SILVER-1, 0),
+                new User("koji1", "코지1", "p1", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER-1, 0),
                 new User("koji2", "코지2", "p2", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER, 0),
                 new User("koji3", "코지3", "p3", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD-1),
                 new User("koji4", "코지4", "p4", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD),
                 new User("koji5", "코지5", "p5", Level.GOLD, 100, Integer.MAX_VALUE)
         );
     }
+
+    @Test
+    public void upgradeAllOrNothing() {
+        TestUserService testUserService = new TestUserService(users.get(3).getId());
+        testUserService.setUserDao(this.userDao);
+        testUserService.setUserLevelUpgradePolicy(new UserLevelUpgradeBasicPolicy());
+        userDao.deleteAll();
+        for(User user : users) userDao.add(user);
+
+        try {
+            testUserService.upgradeLevels();
+            fail("TestUserServiceException expected");
+        } catch (TestUserServiceException e) {
+        }
+
+        checkLevel(users.get(1), false);
+    }
+
+
+
+    @Test
+    public void bean() {
+        assertThat(this.userService, is(notNullValue()));
+    }
+
+
 
     @Test
     public void upgradeLevels() {
