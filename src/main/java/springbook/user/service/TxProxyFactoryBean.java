7bb1a54113968ca37810d50f64a0f139392ff12a
koji <koji4321@naver.com>
2023-03-21 17:37:03 +0900

    6.9
    [기능 추가]
    - TransactionHandler 적용한 TxProxyFactoryBean 등록

---
+++ src/main/java/springbook/user/service/TxProxyFactoryBean.java
@@ -0,0 +1,53 @@
+package springbook.user.service;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.lang.reflect.Proxy;
+
+public class TxProxyFactoryBean implements FactoryBean<Object> {
+
+    Object target;
+    PlatformTransactionManager transactionManager;
+    String pattern;
+    Class<?> serviceInterface;
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public void setServiceInterface(Class<?> serviceInterface) {
+        this.serviceInterface = serviceInterface;
+    }
+
+    @Override
+    public Object getObject() throws Exception {
+        TransactionHandler transactionHandler = new TransactionHandler();
+        transactionHandler.setTransactionManager(transactionManager);
+        transactionHandler.setPattern("");
+        transactionHandler.setTarget(target);
+        return Proxy.newProxyInstance(
+                this.getClass().getClassLoader(),
+                new Class[] { serviceInterface },
+                transactionHandler
+        );
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return serviceInterface;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+}
