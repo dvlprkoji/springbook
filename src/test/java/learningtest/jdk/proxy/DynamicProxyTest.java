00be5254b737cbf412b98a2ff381e3691a409a8b
koji <koji4321@naver.com>
2023-03-24 22:31:13 +0900

    6.11
    [문제점]
    - JDK 다이나믹 프록시, 프록시 팩토리 빈을 사용할 경우 메소드에 부가기능 추가는 가능하지만 클래스에 부가기능 추가는 불가능하다
    - 하나의 타깃에 여러 개의 부가기능을 추가할 때마다 설정파일에 라인이 추가된다
    - 프록시 팩토리 빈을 등록할 때마다 TransactionHandler를 새로 만들어야 한다
    
    [해결 방법]
    - Spring이 제공하는 ProxyFactory를 사용한다

---
+++ src/test/java/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -0,0 +1,112 @@
+package learningtest.jdk.proxy;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Locale;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DynamicProxyTest {
+
+    public interface Hello {
+        String sayHello(String name);
+        String sayHi(String name);
+        String sayThankYou(String name);
+    }
+
+    public class HelloTarget implements Hello {
+        @Override
+        public String sayHello(String name) {
+            return "Hello " + name;
+        }
+
+        @Override
+        public String sayHi(String name) {
+            return "Hi " + name;
+        }
+
+        @Override
+        public String sayThankYou(String name) {
+            return "Thank You " + name;
+        }
+    }
+
+    public class HelloUpperClass implements Hello {
+
+        private Hello hello;
+
+        public void setHello(Hello hello) {
+            this.hello = hello;
+        }
+
+        @Override
+        public String sayHello(String name) {
+            return hello.sayHello(name).toUpperCase(Locale.ROOT);
+        }
+
+        @Override
+        public String sayHi(String name) {
+            return hello.sayHi(name).toUpperCase(Locale.ROOT);
+        }
+
+        @Override
+        public String sayThankYou(String name) {
+            return hello.sayThankYou(name).toUpperCase(Locale.ROOT);
+        }
+    }
+
+    public class UpperCaseHandler implements InvocationHandler {
+
+        Hello target;
+
+        public UpperCaseHandler(Hello target) {
+            this.target = target;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            String ret = (String) method.invoke(target, args);
+            return ret.toUpperCase(Locale.ROOT);
+        }
+    }
+
+
+    @Test
+    public void simpleProxy() {
+        Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{Hello.class},
+                new UpperCaseHandler(new HelloTarget())
+        );
+
+    }
+
+    @Test
+    public void proxyFactoryBean() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+        pfBean.addAdvice(new UppercaseAdvice());
+
+        Hello proxiedHello = (Hello) pfBean.getObject();
+        assertThat(proxiedHello.sayHello("Koji"), is("HELLO KOJI"));
+        assertThat(proxiedHello.sayHi("Koji"), is("HI KOJI"));
+        assertThat(proxiedHello.sayThankYou("Koji"), is("THANK YOU KOJI"));
+    }
+
+    public static class UppercaseAdvice implements MethodInterceptor {
+
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            String ret = (String) invocation.proceed();
+            return ret.toUpperCase(Locale.ROOT);
+        }
+    }
+
+}
