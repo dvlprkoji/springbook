e8ff0f78af093f5d44dbd129b9f1fac27e204bff
koji <koji4321@naver.com>
2023-04-17 21:18:25 +0900

    7.8
    [문제점]
    - 디폴트라고 해도 좋을 만큼 기본적으로 사용될 가능성이 있는 구현 클래스가 존재하는데 xml 설정파일을 통해 DI 한다
    
    [해결]
    - 디폴트 구현 클래스를 만들어 코드 내에서 직접 주입하도록 한다
    - 만약 DI 할 프로퍼티가 있다면 헤당 프로퍼티만 xml 설정파일에서 주입하도록 한다

---
+++ src/main/java/springbook/user/sqlservice/BaseSqlService.java
@@ -0,0 +1,31 @@
+package springbook.user.sqlservice;
+
+import javax.annotation.PostConstruct;
+
+public class BaseSqlService implements SqlService {
+
+    protected SqlReader sqlReader;
+    protected SqlRegistry sqlRegistry;
+
+    public void setSqlReader(SqlReader sqlReader) {
+        this.sqlReader = sqlReader;
+    }
+
+    public void setSqlRegistry(SqlRegistry sqlRegistry) {
+        this.sqlRegistry = sqlRegistry;
+    }
+
+    @PostConstruct
+    public void loadSql() {
+        this.sqlReader.read(this.sqlRegistry);
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        try {
+            return this.sqlRegistry.findSql(key);
+        } catch (SqlNotFoundException e) {
+            throw new SqlRetrievalFailureException(e);
+        }
+    }
+}
---
+++ src/main/java/springbook/user/sqlservice/HashMapSqlRegistry.java
@@ -0,0 +1,22 @@
+package springbook.user.sqlservice;
+
+import java.util.HashMap;
+
+public class HashMapSqlRegistry implements SqlRegistry {
+
+    private HashMap<String, String> sqlMap = new HashMap<String, String>();
+
+    @Override
+    public String findSql(String key) throws SqlNotFoundException {
+        String sql = sqlMap.get(key);
+        if (sql == null) {
+            throw new SqlNotFoundException(key + "를 이용해서 SQL을 찾을 수 없습니다");
+        } else return sql;
+
+    }
+
+    @Override
+    public void registerSql(String key, String sql) {
+        sqlMap.put(key, sql);
+    }
+}
