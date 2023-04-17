e1c5fdab22e7455dff439a978db627aacfd394fc
koji <koji4321@naver.com>
2023-04-17 22:12:53 +0900

    7.9
    [문제점]
    - JAXB 이외의 기술을 사용해 Unmarshalling 을 사용할 수 없다
    
    [해결]
    - org.springframework.oxm 패키지에서 추상화 클래스인 Unmarshaller 를 사용한다

---
+++ src/main/java/springbook/learningtest/spring/oxm/OxmTest.java
@@ -0,0 +1,42 @@
+package springbook.learningtest.spring.oxm;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.XmlMappingException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.user.sqlservice.jaxb.SqlType;
+import springbook.user.sqlservice.jaxb.Sqlmap;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/OxmTest-context.xml")
+public class OxmTest {
+
+    @Autowired
+    private Unmarshaller unmarshaller;
+
+    @Test
+    public void unmarshallerSqlMap() throws XmlMappingException, IOException {
+        Source xmlSource = new StreamSource(getClass().getResourceAsStream("/sqlmap.xml"));
+        Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(xmlSource);
+
+        List<SqlType> sqlList = sqlmap.getSql();
+        assertThat(sqlList.get(0).getKey(), CoreMatchers.is("userAdd"));
+        assertThat(sqlList.get(1).getKey(), CoreMatchers.is("userGet"));
+        assertThat(sqlList.get(2).getKey(), CoreMatchers.is("userGetAll"));
+    }
+
+
+}
+
