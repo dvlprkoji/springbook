c54b0a7b03f6659067297721a0d18c2745eede1a
koji <koji4321@naver.com>
2023-04-26 21:09:20 +0900

    7.19
    [기능 추가]
    - Xml 설정파일의 dataSource 를 어노테이션 기반의 설정파일로 이동

--- src/main/java/springbook/config/TestApplicationContext.java
+++ src/main/java/springbook/config/TestApplicationContext.java
@@ -1,10 +1,26 @@
 package springbook.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ImportResource("/applicationContext.xml")
 public class TestApplicationContext {
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/springbook");
+        dataSource.setUsername("springbook");
+        dataSource.setPassword("Springbook!");
+
+        return dataSource;
+    }
 
 }
