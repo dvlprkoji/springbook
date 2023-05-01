067848655b48a3881b2928eca4cacf48575349fd
koji <koji4321@naver.com>
2023-05-01 14:35:01 +0900

    7.26
    [문제점]
    - DataSource 설정값이 하드코딩 되어있다
    - 테스트, 운영 등 환경에 따라 DataSource 값이 변경될 수 있다
    
    [해결 방법]
    - database.properties 파일을 @PropertySource 로 import 하여 Environment 에 Autowire 한다

--- src/main/java/springbook/config/AppContext.java
+++ src/main/java/springbook/config/AppContext.java
@@ -2,6 +2,7 @@ package springbook.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -16,22 +17,31 @@ import springbook.user.service.UserService;
 import springbook.user.service.UserServiceTest;
 
 import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "springbook.user")
 @Import({SqlServiceContext.class})
+@PropertySource("/database.properties")
 public class AppContext {
+
+    @Autowired
+    Environment env;
 
     //  db
     @Bean
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 
-        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
-        dataSource.setUrl("jdbc:mysql://localhost/springbook");
-        dataSource.setUsername("springbook");
-        dataSource.setPassword("Springbook!");
+        try{
+            dataSource.setDriverClass((Class<? extends Driver>) Class.forName(env.getProperty("db.driverClass")));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
 
         return dataSource;
     }
